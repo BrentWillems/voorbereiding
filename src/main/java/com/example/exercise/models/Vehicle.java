d8a8bf41b1e6f65626715021d17fd2a2de37a0af
brent <brent.willems.bw@gmail.com>
2019-07-16 00:48:30 +0200

    fixed missing annotations and date format

--- src/main/java/com/example/exercise/models/Vehicle.java
+++ src/main/java/com/example/exercise/models/Vehicle.java
@@ -1,9 +1,14 @@
 package com.example.exercise.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Data
 @Entity
+@NoArgsConstructor
 public class Vehicle {
 
     @Id
@@ -15,14 +20,10 @@ public class Vehicle {
 
     private String name;
 
-    public Vehicle() {
-    }
-
     public Vehicle(int id, int currentMilage, int hp, String name) {
         this.id = id;
         this.currentMilage = currentMilage;
         this.hp = hp;
         this.name = name;
     }
-
 }
